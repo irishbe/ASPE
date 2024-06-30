26ea6523ce9e8f63243bcf0b21429f00455195a9
WhenDavi <92559102+WhenDavi@users.noreply.github.com>
2024-06-30 23:24:03 +0100

    Add files via upload

--- Programas Independientes/PonderadoNotas.cpp
+++ Programas Independientes/PonderadoNotas.cpp
@@ -28,17 +28,19 @@ struct Curso{
 int cantNotas;
 
 int main(){
+	SetConsoleOutputCP(CP_UTF8);
+	
 	int opc;
 
 	string tituloPonderado = "PONDERADO DE NOTAS";
 	string menuPonderado[8] = {
-		"1. Leer Notas",
-		"2. Crear Notas",
-		"3. Actualizar Notas",
-		"4. Eliminar Notas",
-		"5. Ordenar datos",
-		"6. Analisis estadistico",
-		"7. Eliminar todas las notas guardadas",		
+		"1. Leer cursos",
+		"2. Crear curso",
+		"3. Actualizar curso",
+		"4. Eliminar curso",
+		"5. Ordenar notas de cursos",
+		"6. Analisis estadistico Notas / Cursos",
+		"7. Eliminar todas los cursos guardados",		
 		"\n\tESC. Salir"
 	};
 
@@ -136,65 +138,77 @@ void leerNotas(){
 
 void crearNotas() {
     FILE* archivo = fopen(archivoNotas, "a+b");
-	Curso c;
-
-    char res, agregar;
-    cout<<"\n\n\t"; colorTexto("CREACIÓN DE CURSOS", blancoBrillante); cout<<"\n\n";
     
-    do {
-        fflush(stdin);
-        cout<< "\n\tIngrese nombre del curso: ";
-        cin.getline(c.nombre, 31);
-
-		cout<<"\n\tEmpezar a agregar notas en "<<c.nombre<<"? (s/n): "; cin>>agregar;
+    char res, agregar;
+    int i=1;
+    cout<<"\n\n\t"; colorTexto("CREACIÓN DE CURSOS", blancoBrillante);
+    
+    do{
+    	Curso c;
+    	cout<<"\n\n\tCurso "<<i;
+    	
+    	fflush(stdin);
+		cout << "\n\n\tIngrese nombre del curso: "; cin.getline(c.nombre, 31, '\n'); cin.clear();
+		
+    	fflush(stdin);
+    	cout << "\tEmpezar a agregar notas en " << c.nombre << "? (s/n): "; cin >> agregar;
 		
 		int opc=1;
 		while( (agregar == 's' || agregar == 'S') && opc != 0){
+			
 			system("cls");
-			string opcionesNotas [5] = {"1. Nota PC1", "2. Nota Parcial", "3. Nota PC2", "4. Nota Final", "ESC. Salir"};
-			string titulo = "> Nombre del curso: " + string(c.nombre) + "\n\n\tAgregar nota en...";
+			string opcionesNotas [5] = {"1. Nota PC1", "2. Nota Parcial", "3. Nota PC2", "4. Nota Final", "\n\tESC. Salir"};
+		
+			//Impresion de valores segun si esta llenado o no la nota
+			string pc1 = "\n\n\tPC1: " + (c.pc1.llenado ? to_string(c.pc1.valor) : "---");
+			string parcial = " | Parcial: " + (c.parcial.llenado ? to_string(c.parcial.valor) : "---");
+			string pc2 = " | PC2: " + (c.pc2.llenado ? to_string(c.pc2.valor) : "---");
+			string final = " | Final: " + (c.final.llenado ? to_string(c.final.valor) : "---");
+		
+			//Concatenacion de strings
+			string titulo = "> Nombre del curso: " + string(c.nombre) + pc1 + parcial + pc2 + final + "\n\n\tAgregar nota en...";
 			
 			opc = opcionSeleccionada(opcionesNotas, titulo, 5);
 			
 			switch(opc){
 				case 1:{
 					do{
-			            cout<<"\n\tIngrese la nota de la PC1:   ---> ";
-			            cin>>c.pc1.valor;
-			            if(c.pc1.valor < 0 || c.pc1.valor > 20 ){
-			                colorTextoFondo("\n\tNota fuera de rango. Ingrese un valor entre 0 y 20.\n\n", blancoBrillante, rojo);
+						cin.clear();
+			            cout<<"\n\tIngrese la nota de la PC1:   ---> "; cin>>c.pc1.valor;
+			            if(c.pc1.valor < 0 || c.pc1.valor > 20 || cin.fail() ){
+			                cout<<"\n\t"; colorTextoFondo("Nota fuera de rango. Ingrese un valor entre 0 y 20.", blancoBrillante, rojo); cout<<"\n\n";
 			            }
-			        }while(c.pc1.valor<0 || c.pc1.valor>20); c.pc1.llenado = true;
+			        }while( c.pc1.valor<0 || c.pc1.valor>20 || cin.fail() ); c.pc1.llenado = true;
 					break;
 				}
 				case 2:{
 					do {
-			            cout<<"\tIngrese la nota del parcial: ---> ";
-			            cin>>c.parcial.valor;
-			            if(c.parcial.valor<0 || c.parcial.valor>20){
-			                colorTextoFondo("\n\tNota fuera de rango. Ingrese un valor entre 0 y 20.\n\n", blancoBrillante, rojo);
+						cin.clear();
+			            cout<<"\n\tIngrese la nota del parcial: ---> ";  cin>>c.parcial.valor;
+			            if(c.parcial.valor<0 || c.parcial.valor>20 || cin.fail()){
+			                cout<<"\n\t"; colorTextoFondo("Nota fuera de rango. Ingrese un valor entre 0 y 20.", blancoBrillante, rojo); cout<<"\n\n";
 			            }
-			        } while(c.parcial.valor<0 || c.parcial.valor>20); c.parcial.llenado = true;
+			        } while(c.parcial.valor<0 || c.parcial.valor>20 || cin.fail()); c.parcial.llenado = true;
 					break;
 				}
 				case 3:{
 					do {
-			            cout<<"\tIngrese la nota de la PC2:   ---> ";
-			            cin>>c.pc2.valor;
-			            if (c.pc2.valor<0 || c.pc2.valor>20) {
-			                colorTextoFondo("\n\tNota fuera de rango. Ingrese un valor entre 0 y 20.\n\n", blancoBrillante, rojo);
+						cin.clear();
+			            cout<<"\n\tIngrese la nota de la PC2:   ---> "; cin>>c.pc2.valor;
+			            if (c.pc2.valor<0 || c.pc2.valor>20|| cin.fail()) {
+			                cout<<"\n\t"; colorTextoFondo("Nota fuera de rango. Ingrese un valor entre 0 y 20.", blancoBrillante, rojo); cout<<"\n\n";
 			            }
-			        } while(c.pc2.valor<0 || c.pc2.valor>20); c.pc2.llenado = true;
+			        } while(c.pc2.valor<0 || c.pc2.valor>20|| cin.fail()); c.pc2.llenado = true;
 					break;
 				}
 				case 4:{
 					do {
-			            cout << "\tIngrese la nota del final:   ---> ";
-			            cin >> c.final.valor;
-			            if (c.final.valor<0 || c.final.valor>20){
-			                colorTextoFondo("\n\tNota fuera de rango. Ingrese un valor entre 0 y 20.\n\n", blancoBrillante, rojo);
+						cin.clear();
+			            cout << "\n\tIngrese la nota del final:   ---> "; cin >> c.final.valor;
+			            if (c.final.valor<0 || c.final.valor>20|| cin.fail()){
+			                cout<<"\n\t"; colorTextoFondo("Nota fuera de rango. Ingrese un valor entre 0 y 20.", blancoBrillante, rojo); cout<<"\n\n";
 			            }
-			        } while(c.final.valor<0 || c.final.valor>20); c.final.llenado = true;
+			        } while(c.final.valor<0 || c.final.valor>20|| cin.fail()); c.final.llenado = true;
 					break;
 				}
 				case 0:{
@@ -206,11 +220,15 @@ void crearNotas() {
 	    c.promedio=(c.pc1.valor*0.2)+(c.parcial.valor*0.3)+(c.pc2.valor*0.2)+(c.final.valor*0.3);
 		fwrite(&c,sizeof(Curso),1,archivo);
 		
-	    cout<<"\n\tDesea ingresar notas de otro curso? (s/n): ";
+		fflush(stdin);
+	    cout<<"\tIngresar crear otro curso? (s/n): ";
 		cin>>res;
-    } while(res=='s' || res=='S');
+		i++;
+		
+    }while(res=='s' || res=='S');
 
-	colorTextoFondo("\n\tDatos guardados exitosamente!!!\n\n", blancoBrillante, verde);
+	cout<<"\n\t"; colorTextoFondo("Datos guardados exitosamente!!!", blancoBrillante, verde); cout<<"\n\n\t";
+	
 	system("pause"); system("cls");
 		
     fclose(archivo);
@@ -228,8 +246,17 @@ void actualizarNotas(){
     }
 
     int id = 0;
-    cout << "Ingrese ID del curso para modificar: ";
-    cin >> id;
+    do{
+		cin.clear();
+		fflush(stdin);
+		
+		cout<<"\tId de la nota que va actualizar: ";cin>>id;
+		if(id<0 || id>(cantNotas-1) || cin.fail() ){
+			cout<<"\n\t";
+			colorTextoFondo("Id no existente", blancoBrillante, rojo);
+			cout<<"\n\n";
+		}
+	}while(id<0 || id > (cantNotas-1) || cin.fail() );
     
     // Mueve el puntero del archivo al registro deseado
     fseek(archivo, id * sizeof(Curso), SEEK_SET);
@@ -238,53 +265,61 @@ void actualizarNotas(){
     fread(&c, sizeof(Curso), 1, archivo);
     
     // Actualiza solo las notas, el nombre del curso se mantiene
-    int opc=1;
+    int opc;
 	do{
 		system("cls");
-		string opcionesNotas [5] = {"1. Nota PC1", "2. Nota Parcial", "3. Nota PC2", "4. Nota Final", "ESC. Salir"};
-		string titulo = "> Nombre del curso: " + string(c.nombre) + "\n\n\tAgregar nota en...";
-			
+		string opcionesNotas [5] = {"1. Nota PC1", "2. Nota Parcial", "3. Nota PC2", "4. Nota Final", "\n\tESC. Salir"};
+		
+		//Impresion de valores segun si esta llenado o no la nota
+		string pc1 = "\n\n\tPC1: " + (c.pc1.llenado ? to_string(c.pc1.valor) : "---");
+		string parcial = " | Parcial: " + (c.parcial.llenado ? to_string(c.parcial.valor) : "---");
+		string pc2 = " | PC2: " + (c.pc2.llenado ? to_string(c.pc2.valor) : "---");
+		string final = " | Final: " + (c.final.llenado ? to_string(c.final.valor) : "---");
+		
+		//Concatenacion de strings
+		string titulo = "> Nombre del curso: " + string(c.nombre) + pc1 + parcial + pc2 + final + "\n\n\tAgregar nota en...";
+				
 		opc = opcionSeleccionada(opcionesNotas, titulo, 5);
 			
 		switch(opc){
 			case 1:{
 					do{
-			            cout<<"\n\tIngrese la nota de la PC1:   ---> ";
-			            cin>>c.pc1.valor;
-			            if(c.pc1.valor < 0 || c.pc1.valor > 20 ){
-			                colorTextoFondo("\n\tNota fuera de rango. Ingrese un valor entre 0 y 20.\n\n", blancoBrillante, rojo);
+						cin.clear();
+			            cout<<"\n\tIngrese la nota de la PC1:   ---> "; cin>>c.pc1.valor;
+			            if(c.pc1.valor < 0 || c.pc1.valor > 20 || cin.fail()){
+			                cout<<"\n\t"; colorTextoFondo("Nota fuera de rango. Ingrese un valor entre 0 y 20.", blancoBrillante, rojo); cout<<"\n\n";
 			            }
-			        }while(c.pc1.valor<0 || c.pc1.valor>20); c.pc1.llenado = true;
+			        }while(c.pc1.valor<0 || c.pc1.valor>20 || cin.fail()); c.pc1.llenado = true;
 					break;
 				}
 			case 2:{
 					do {
-			            cout<<"\tIngrese la nota del parcial: ---> ";
-			            cin>>c.parcial.valor;
-			            if(c.parcial.valor<0 || c.parcial.valor>20){
-			                colorTextoFondo("\n\tNota fuera de rango. Ingrese un valor entre 0 y 20.\n\n", blancoBrillante, rojo);
+						cin.clear();
+			            cout<<"\n\tIngrese la nota del parcial: ---> "; cin>>c.parcial.valor;
+			            if(c.parcial.valor<0 || c.parcial.valor>20 || cin.fail()){
+			                cout<<"\n\t"; colorTextoFondo("Nota fuera de rango. Ingrese un valor entre 0 y 20.", blancoBrillante, rojo); cout<<"\n\n";
 			            }
-			        } while(c.parcial.valor<0 || c.parcial.valor>20); c.parcial.llenado = true;
+			        } while(c.parcial.valor<0 || c.parcial.valor>20 || cin.fail()); c.parcial.llenado = true;
 					break;
 				}
 			case 3:{
 					do {
-			            cout<<"\tIngrese la nota de la PC2:   ---> ";
-			            cin>>c.pc2.valor;
-			            if (c.pc2.valor<0 || c.pc2.valor>20) {
-			                colorTextoFondo("\n\tNota fuera de rango. Ingrese un valor entre 0 y 20.\n\n", blancoBrillante, rojo);
+						cin.clear();
+			            cout<<"\n\tIngrese la nota de la PC2:   ---> "; cin>>c.pc2.valor;
+			            if (c.pc2.valor<0 || c.pc2.valor>20 || cin.fail()) {
+			                cout<<"\n\t"; colorTextoFondo("Nota fuera de rango. Ingrese un valor entre 0 y 20.", blancoBrillante, rojo); cout<<"\n\n";
 			            }
-			        } while(c.pc2.valor<0 || c.pc2.valor>20); c.pc2.llenado = true;
+			        } while(c.pc2.valor<0 || c.pc2.valor>20 || cin.fail()); c.pc2.llenado = true;
 					break;
 				}
 			case 4:{
 				do {
-		            cout << "\tIngrese la nota del final:   ---> ";
-		            cin >> c.final.valor;
-		            if (c.final.valor<0 || c.final.valor>20){
-		               colorTextoFondo("\n\tNota fuera de rango. Ingrese un valor entre 0 y 20.\n\n", blancoBrillante, rojo);
+					cin.clear();
+		            cout << "\n\tIngrese la nota del final:   ---> "; cin >> c.final.valor;
+		            if (c.final.valor<0 || c.final.valor>20 || cin.fail()){
+		               cout<<"\n\t"; colorTextoFondo("Nota fuera de rango. Ingrese un valor entre 0 y 20.", blancoBrillante, rojo); cout<<"\n\n";
 		            }
-		        } while(c.final.valor<0 || c.final.valor>20); c.final.llenado = true;
+		        } while(c.final.valor<0 || c.final.valor>20 || cin.fail()); c.final.llenado = true;
 				break;
 			}
 			case 0:{
@@ -308,7 +343,7 @@ void eliminarNotas(){
 	//Visualizacion de las tareas activas
 	leerNotas();
 	
-	cout<<"\n\n\t"; colorTexto("ELIMINAR NOTAS", blancoBrillante); cout<<"\n\n";
+	cout<<"\n\n\t" ; colorTexto("ELIMINAR NOTAS", blancoBrillante); cout<<"\n\n";
 	
 	//Apertura de archivos y definicion de variables
 	short id=0, id_borrar;
@@ -317,15 +352,24 @@ void eliminarNotas(){
 	Curso c;
 	
 	//Solicitud del id
-	fflush(stdin);
-	cout<<"Id del curso a a eliminar: ";cin>>id_borrar;                                
+	do{
+		cin.clear();
+		fflush(stdin);
+		
+		cout<<"\tId de la nota a eliminar: ";cin>>id_borrar;
+		if(id_borrar<0 || id_borrar > (cantNotas-1) || cin.fail() ){
+			cout<<"\n\t";
+			colorTextoFondo("Id no existente", blancoBrillante, rojo);
+			cout<<"\n\n";
+		}
+	}while(id_borrar<0 || id_borrar > (cantNotas-1) || cin.fail() );                           
 	
 	//Duplicado de las tareas del archivo original al temp a excepcion del id
 	for(id=0; id < cantNotas ; id++){
 		fread(&c, sizeof(Curso), 1, archivo);
 		
 		if (id_borrar != id){
-			fwrite(&c, sizeof(Curso), 1, archivo);
+			fwrite(&c, sizeof(Curso), 1, temp);
 		}
 	}
 
@@ -336,7 +380,7 @@ void eliminarNotas(){
 	temp = fopen("temp.dat", "r+b");
 	
 	for(id=0; id < cantNotas - 1; id++){
-		fread(&c, sizeof(Curso), 1, archivo);
+		fread(&c, sizeof(Curso), 1, temp);
 		fwrite(&c, sizeof(Curso), 1, archivo);
 	}
 	
@@ -349,17 +393,18 @@ void eliminarNotas(){
 }
 
 void ordenarNotas(){
-	Curso c, aux;
 	bool ordenado, condicion, opc_invalida;
 	char opc;
 	FILE *archivo = fopen(archivoNotas, "r+b");
 	
 	do{
 		//MENU DE OPCIONES PARA LOS METODOS DE ORDENAMIENTO
+		Curso c, aux;
+		
 		system("CLS");
 		
 		leerNotas();
-		cout<<"\n\n\t"; colorTexto("MÉTODOS DE ORDENAMIENTO", blancoBrillante); cout<<"\n\n";
+		cout<<"\n\t"; colorTexto("MÉTODOS DE ORDENAMIENTO", blancoBrillante); cout<<"\n\n";
 		
 		cout<<"1. Ordenamiento por nombre del curso"<<endl;
 		cout<<"2. Ordenamiento de notas PC1 (menor a mayor)"<<endl;
@@ -384,8 +429,7 @@ void ordenarNotas(){
 					fread(&aux, sizeof(Curso), 1, archivo);
 					
 					switch(opc){
-						//ORDENAMIENTO POR FECHA DE CREACION
-						case '1': condicion = ( tolower(c.nombre[0]) > tolower(c.nombre[0]) ); break;
+						case '1': condicion = ( tolower(c.nombre[0]) > tolower(aux.nombre[0]) ); break;
 						
 						//ORDENAMIENTO DE NOTAS
 						case '2': condicion = ( c.pc1.valor > aux.pc1.valor ); break;
@@ -399,7 +443,8 @@ void ordenarNotas(){
 						case '6': condicion = ( c.promedio > aux.promedio ); break;
 						
 						default: opc_invalida = true; 
-						colorTextoFondo("\n\tOpcion invalida\n\n", blancoBrillante, rojo); system("pause");
+						cout<<"\n\t"; colorTextoFondo("Opcion invalida", blancoBrillante, rojo); cout<<"\n\n";
+						system("pause");
 					}
 					
 					//Control de entrada no deseada
@@ -425,8 +470,7 @@ void ordenarNotas(){
 }
 
 void estadisticasNotas(){
-	leerNotas();
-	cout<<"\n\n\t"; colorTexto("ANÁLISIS ESTADÍSTICO", blancoBrillante); cout<<"\n\n";
+	cout<<"\n\t"; colorTexto("Resultados de las Notas", blancoBrillante); cout<<"\n\n";
 		
     FILE *archivo = fopen(archivoNotas, "rb");
     Curso c;
@@ -444,6 +488,7 @@ void estadisticasNotas(){
             suma_promedios += c.promedio;
             total_cursos++;
         }
+        cout<<"- Promedio de "<<c.nombre<<": "<<c.promedio<<endl;
     }
 
     if (total_cursos > 0) {
@@ -451,26 +496,7 @@ void estadisticasNotas(){
     }
 
     fclose(archivo);
-    
-	cout<<"/////////////////////////////////"<<endl<<endl;
 	cout<<"\nPromedio ponderado: "<<promedio_ponderado<<endl<<endl;
-
-    string evaluacion;
-    if (promedio_ponderado >= 18) {
-        evaluacion = "Excelente";
-    } else if (promedio_ponderado >= 15) {
-        evaluacion = "Bueno";
-    } else if (promedio_ponderado >= 12) {
-        evaluacion = "Regular";
-    } else if (promedio_ponderado >= 10.5) {
-        evaluacion = "Falta repasar";
-    } else if (promedio_ponderado >= 8) {
-        evaluacion = "Necesita aprender mas";
-    } else {
-        evaluacion = "Tiene que repasar todo";
-    }
-
-    cout << "Evaluacion: "<<evaluacion<<endl<<endl;
 
     // Gráfico de barras
     cout << "Grafico de barras: "<<endl<<endl;
@@ -484,16 +510,5 @@ void estadisticasNotas(){
     }
     cout<<"|"<<endl;
     cout<<"0         10         20" <<endl<<endl;;
-    cout<<"/////////////////////////////////"<<endl<<endl;
     cout<<endl<<endl;
-    
-    system("pause");
-    
-    FILE* archivo2=fopen(archivoEstadisticasNotas,"a+b");        
-
-	promedio_ponderado;
-	evaluacion;
-	colorTextoFondo("\n\tDatos guardados en analisis.dat", blancoBrillante, verde);
-	cout<<endl<<endl;
-	fclose(archivo2);
 }
